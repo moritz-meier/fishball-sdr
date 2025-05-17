5edb8e3459a03ae24fe6ed14f24d3b5fc1c9f3ba
Moritz Meier <mam2911+git@pm.me>
2025-05-17 18:39:08 +0200

    fit dtb

--- pkgs/fishball-sdr-fw/package.nix
+++ pkgs/fishball-sdr-fw/package.nix
@@ -10,6 +10,7 @@
 let
   kernel = "${nixosConfig.config.system.build.kernel}/zImage";
   initrd = "${nixosConfig.config.system.build.netbootRamdisk}/initrd.zst";
+  dts = ./linux-dt.dts;
   inherit (lib.strings) escapeShellArg;
 in
 stdenvNoCC.mkDerivation {
@@ -31,7 +32,8 @@ stdenvNoCC.mkDerivation {
     ln --symbolic -- ${initrd} ./
 
     # copy devicetree
-    # TODO
+    ln --symbolic -- ${dts} ./
+    dtc -I dts -O dtb -o system.dtb ${dts}
 
     # make initrd image
     mkimage -A arm -T ramdisk -d  ${nixosConfig.config.system.build.netbootRamdisk}/initrd.* ./initrd.img
@@ -39,7 +41,8 @@ stdenvNoCC.mkDerivation {
     # make FIT image
     substitute ${./boot.its} ./boot.its \
       --subst-var-by kernel ${escapeShellArg kernel} \
-      --subst-var-by initrd ${escapeShellArg initrd}
+      --subst-var-by initrd ${escapeShellArg initrd} \
+      --subst-var-by dtb "$out/system.dtb"
     mkimage -f ./boot.its ./fit-image.fit
   '';
 }
