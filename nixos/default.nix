cb8380bddc391af4dd5d721b557d19fa360e2b2d
Moritz Meier <git@mam2911.slmail.me>
2026-02-21 19:49:50 +0100

    wip

--- nixos/default.nix
+++ nixos/default.nix
@@ -8,10 +8,8 @@
 {
   imports = [
     ./kernel.nix
-    # (modulesPath + "/installer/netboot/netboot.nix")
-    ./standalone-ramdisk.nix
+    (modulesPath + "/installer/netboot/netboot.nix")
     (modulesPath + "/profiles/perlless.nix")
-    ./debloat.nix
     ./config.nix
   ];
 
@@ -21,13 +19,15 @@
     nixpkgs.buildPlatform = lib.mkForce "x86_64-linux";
     nixpkgs.hostPlatform = lib.mkForce "armv7l-linux";
 
+    boot.initrd.compressor = "cat";
+
     system.build.sdCard = pkgs.pkgsBuildBuild.linkFarmFromDrvs "sdcard" [
       config.system.build.kernel
       config.system.build.kernel.configfile
-      config.system.build.standaloneRamdisk
+      config.system.build.netbootRamdisk
     ];
 
-    system.build.qemuVm = pkgs.pkgsBuildBuild.writeShellApplication {
+    system.build.runQemu = pkgs.pkgsBuildBuild.writeShellApplication {
       name = "run-${config.system.name}-vm";
       checkPhase = if pkgs.stdenv.buildPlatform == pkgs.stdenv.hostPlatform then null else "";
 
@@ -39,11 +39,12 @@
         echo 'launching QEMU'
         ${pkgs.pkgsBuildBuild.qemu_full}/bin/qemu-system-arm \
           -machine virt \
-          -m size=4G -nographic \
+          -m size=2G -smp 2 -nographic \
           -nographic \
           -serial mon:stdio \
           -kernel ${config.system.build.kernel}/zImage \
-          -initrd ${config.system.build.standaloneRamdisk}/initrd
+          -initrd ${config.system.build.netbootRamdisk}/initrd \
+          -append "init=${config.system.build.toplevel}/init"
       '';
     };
   };
--- nixos/kernel.nix
+++ nixos/kernel.nix
@@ -29,121 +29,60 @@
 
     boot.kernelPackages = pkgs.linuxPackagesFor (
       pkgs.linuxPackages.kernel.override {
-        autoModules = false;
-        kernelPreferBuiltin = true;
-        enableCommonConfig = false;
-        defconfig = "tinyconfig";
-        argsOverride = {
-          # https://github.com/wucke13/minimal-nixos/blob/main/pkgs/minimal-linux-kernel.nix
+        # autoModules = false;
+        # kernelPreferBuiltin = true;
+        # enableCommonConfig = false;
+        # defconfig = "tinyconfig";
+        # argsOverride = {
+        # structuredExtraConfig = with lib.kernel; {
+        #   MODULES = yes;
 
-          structuredExtraConfig = with lib.kernel; {
-            # nixos nags about these
-            AUTOFS_FS = yes;
-            MODULES = yes;
-            CRYPTO_HMAC = yes;
-            CRYPTO_SHA256 = yes;
-            SECCOMP = yes;
+        #   AEABI = yes;
+        #   ARCH_VIRT = yes;
+        #   ARM_THUMBEE = yes;
+        #   COMPAT_32BIT_TIME = yes;
+        #   MMU = yes;
+        #   NEON = yes;
+        #   VFP = yes;
 
-            # essentials
-            BINFMT_ELF = yes;
-            BINFMT_SCRIPT = yes; # otherwise shebangs wont work for systemd services
-            BLK_DEV_INITRD = yes;
-            CRYPTO = yes;
-            CRYPTO_USER_API_HASH = yes;
-            FILE_LOCKING = yes; # otherwise libmount fails to updat the userpace mount table
-            MULTIUSER = yes;
-            SMP = yes; # enable multi-core support
+        #   BINFMT_ELF = yes;
+        #   BINFMT_SCRIPT = yes;
+        #   BLK_DEV_INITRD = yes;
 
-            # armv7 specials
-            AEABI = yes; # we assume eabi in the userspace for NixOS on armv7l
-            ARCH_MULTI_V7 = yes; # enable Cortex-A support, avoid µ-controller Linux
-            ARCH_VIRT = yes; # enable virtualization support
-            ARM_THUMBEE = yes; # ThumbEE may be used by our userspace
-            COMPAT_32BIT_TIME = yes; # otherwise glibc's pthread_once exits with exit code 4
-            MMU = yes; # enable MMU
-            NEON = yes; # we assume neon is available
-            VFP = yes; # target is gnueabihf -> hardware floating point unit support
+        #   PRINTK = yes;
+        #   TTY = yes;
 
-            # network
-            INET = yes;
-            IPV6 = yes;
-            NET = yes;
-            NETDEVICES = yes; # otherwise systemd-resolved can't bind using SO_BINDTOIFINDEX
-            PACKET = yes; # otherwise sytemd-networkd fails to acquire DHCP leases
+        #   FILE_LOCKING = yes;
+        #   FUTEX = yes;
 
-            # erofs support
-            EROFS_FS = yes;
-            EROFS_FS_POSIX_ACL = yes;
-            EROFS_FS_XATTR = yes;
+        #   CGROUPS = yes;
+        #   EPOLL = yes;
+        #   EVENTFD = yes;
+        #   FHANDLE = yes;
+        #   INOTIFY_USER = yes;
+        #   SIGNALFD = yes;
+        #   TIMERFD = yes;
 
-            # overlayfs support
-            OVERLAY_FS = yes;
+        #   DEVTMPFS = yes;
+        #   SYSFS = yes;
+        #   PROC_FS = yes;
 
-            # squashfs support
-            BLOCK = yes;
-            BLK_DEV_LOOP = yes;
-            MISC_FILESYSTEMS = yes;
-            SQUASHFS = yes;
-            SQUASHFS_ZSTD = yes;
-            SQUASHFS_CHOICE_DECOMP_BY_MOUNT = yes; # make `mount -o threads=multi` work
+        #   BLOCK = yes;
+        #   BLK_DEV_LOOP = yes;
+        #   MISC_FILESYSTEMS = yes;
 
-            # tmpfs support
-            SHMEM = yes; # required for TMPFS
-            TMPFS = yes;
-            TMPFS_POSIX_ACL = yes;
-            TMPFS_XATTR = yes;
+        #   TMPFS = yes;
+        #   OVERLAY_FS = yes;
 
-            # glibc
-            FUTEX = yes; # for pthreads implementation
+        #   SQUASHFS = yes;
+        #   SQUASHFS_ZSTD = yes;
+        #   SQUASHFS_CHOICE_DECOMP_BY_MOUNT = yes;
 
-            # systemd requirements form the manual
-            DEVTMPFS = yes;
-            CGROUPS = yes;
-            INOTIFY_USER = yes;
-            SIGNALFD = yes;
-            TIMERFD = yes;
-            EPOLL = yes;
-            UNIX = yes;
-            SYSFS = yes;
-            PROC_FS = yes;
-            FHANDLE = yes;
-
-            # systemd goodies from the manual
-            SECCOMP_FILTER = yes;
-            NET_SCHED = yes;
-            NET_SCH_FQ_CODEL = yes;
-            KCMP = yes;
-            EVENTFD = yes; # systemd calls it config_event_fd
-
-            # unofficial systemd requirements
-            POSIX_TIMERS = yes; # required for systemd-update-utmp
-            RSEQ = yes; # used by systemd-update-utmp
-
-            # IO
-            PRINTK = yes;
-            SERIAL_AMBA_PL011 = yes;
-            SERIAL_AMBA_PL011_CONSOLE = yes;
-            TTY = yes;
-
-            # make the kernel behave better as a guest
-            PARAVIRT = yes;
-            VIRTIO = yes;
-            VIRTIO_CONSOLE = yes;
-
-            # virtio networking
-            ETHERNET = yes;
-            PCI = yes;
-            VIRTIO_MENU = yes;
-            VIRTIO_NET = yes;
-            VIRTIO_PCI = yes;
-
-            # xilinx
-            ARCH_ZYNQ = yes;
-            SERIAL_XILINX_PS_UART = yes;
-            SERIAL_XILINX_PS_UART_CONSOLE = yes;
-          };
-          ignoreConfigErrors = false;
-        };
+        #   SERIAL_AMBA_PL011 = yes;
+        #   SERIAL_AMBA_PL011_CONSOLE = yes;
+        # };
+        # ignoreConfigErrors = false;
+        # };
       }
     );
   };
